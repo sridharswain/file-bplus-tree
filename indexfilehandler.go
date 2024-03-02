99751c8ed8f450297bfea5e19e8bc8654dbb05cf
sridhar_swain <sridhar_swain@intuit.com>
2024-03-02 06:10:15 +0530

    cache added

--- indexfilehandler.go
+++ indexfilehandler.go
@@ -5,6 +5,10 @@ import (
 	"cmp"
 	"encoding/json"
 	"os"
+	"strconv"
+	"time"
+
+	"github.com/dgraph-io/ristretto"
 )
 
 const (
@@ -14,9 +18,41 @@ const (
 
 const (
 	METADATA_SIZE    = 1 * 1024
-	INDEX_BLOCK_SIZE = 1 * 1024
-	PAGE_BLOCK_SIZE  = 1 * 1024
+	INDEX_BLOCK_SIZE = 4 * 1024
+	PAGE_BLOCK_SIZE  = 4 * 1024
 )
+
+var (
+	cache *ristretto.Cache = createCache()
+)
+
+func createCache() *ristretto.Cache {
+	cache, err := ristretto.NewCache(&ristretto.Config{
+		NumCounters: 1e7,     // number of keys to track frequency of (10M).
+		MaxCost:     1 << 30, // maximum cost of cache (1GB).
+		BufferItems: 64,      // number of keys per Get buffer.
+	})
+	if err != nil {
+		panic(err)
+	}
+	return cache
+}
+
+func getFromCache(offset int) ([]byte, bool) {
+	data, exists := cache.Get(strconv.Itoa(offset))
+	if !exists {
+		return nil, exists
+	}
+	return data.([]byte), exists
+}
+
+func setInCache(offset int, value []byte) {
+	cache.SetWithTTL(strconv.Itoa(offset), value, int64(len(value)), time.Hour)
+}
+
+func deleteInCache(offset int) {
+	cache.Del(strconv.Itoa(offset))
+}
 
 type PageBlockType struct {
 	PageType string
@@ -46,6 +82,7 @@ func SaveAt[TKey cmp.Ordered, TValue any, TTPage PageBlock[TKey, TValue]](indexN
 	if _, err = f.WriteAt(writeBytes, int64(offset)); err != nil {
 		panic(err)
 	}
+	deleteInCache(offset)
 }
 
 func SaveDataPage[TKey cmp.Ordered, TValue any](indexName string, page *DataPage[TKey, TValue], offset int) {
@@ -63,30 +100,37 @@ func SaveMetadata[TKey cmp.Ordered, TValue any](indexName string, page *BTree[TK
 }
 
 func ReadAt[TKey cmp.Ordered, TValue any, TTPage PageBlock[TKey, TValue]](indexName string, page TTPage, offset int, length int) []byte {
-	file, err := os.OpenFile(indexName, os.O_RDONLY, os.ModePerm)
+	var datatToUnmarshal []byte
+
+	datatToUnmarshal, exists := getFromCache(offset)
+	if !exists {
+		file, err := os.OpenFile(indexName, os.O_RDONLY, os.ModePerm)
+
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		var buffer []byte = make([]byte, length)
+		_, err = file.ReadAt(buffer, int64(offset))
+
+		if err != nil {
+			panic(err)
+		}
+		jsonBytes := buffer[:bytes.IndexByte(buffer, 0)]
+		datatToUnmarshal = make([]byte, len(jsonBytes))
+		copy(datatToUnmarshal, jsonBytes)
+
+		setInCache(offset, datatToUnmarshal)
+	}
+
+	err := json.Unmarshal(datatToUnmarshal, page)
 
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
 
-	var buffer []byte = make([]byte, length)
-	_, err = file.ReadAt(buffer, int64(offset))
-
-	if err != nil {
-		panic(err)
-	}
-	jsonBytes := buffer[:bytes.IndexByte(buffer, 0)]
-	var dataToRead []byte = make([]byte, len(jsonBytes))
-	copy(dataToRead, jsonBytes)
-
-	err = json.Unmarshal(dataToRead, page)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return jsonBytes
+	return datatToUnmarshal
 }
 
 func ReadDataPage[TKey cmp.Ordered, TValue any](tree *BTree[TKey, TValue], page *DataPage[TKey, TValue], offset int) {
--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 )
 
 func main() {
-	tree := New[int, int]("hello", 10)
+	tree := New[int, int]("hello", 128)
 
 	timer := time.Now()
 
-	x:= 1000
-	for i := 0; i < 10000; i++ {
+	x:= 100000
+	for i := 0; i < x; i++ {
 		tree.Put(i, 100)
 	}
 
@@ -23,9 +24,9 @@ func main() {
 	// tree.Put(4, 100)
 	// tree.Put(5, 100)
 	// tree.Put(6, 100)
-	// // tree.Put(7, 100)
+	// tree.Put(7, 100)
 	// tree.Put(8, 100)
 	// tree.Put(9, 100)
 	// tree.Put(10, 100)
-	log.Println(fmt.Sprintf("Taken taken to push %s data : %s", x, time.Since(timer)))
+	log.Println(fmt.Sprintf("Taken taken to push %s data : %s", strconv.Itoa(x), time.Since(timer)))
 }
